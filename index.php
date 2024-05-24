918048f61fc9752c1b699bef17ed3ce146c119dd
juansprz <juanperez@Mac-mini-de-Sebastian.local>
2024-05-24 16:04:40 -0500

    Historia de usuario 1

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<title>Centro Internacional Cristiano</title>
+	<link rel="shortcut icon" href="/img/icons/icon.ico">
+	<link rel="stylesheet" href="/css/index.css">
+</head>	
+
+<body>
+	<header>
+		<?php include ('menu/menu.html');?>
+	</header>
+
+		<?php include ('slider.html');?> 
+
+
+	<div class="historia">
+		<div class="img-historia"><div id="img-historia"></div></div>
+		<div class="informacion-historia">
+			<h1 class="title red right wow animated bounceInLeft" style="size: '58'">UNA GRAN FAMILIA</h1>
+			<br>
+			<h4 class="text light justify wow animated bounceInRight">
+			CIC nace por una palabra que Dios le dio a los pastores Ricardo y Diana Álvarez en el año 2002, donde Dios les dijo que multitudes les iban a ser entregadas para que ellos las pastorearan con amor y cuidado. En Diciembre del 2003 iniciamos nuestras reuniones con mas de 80 personas, ese día se impartió la visión de lo que sería Centro Internacional Cristiano.
+			Al día de hoy somos una familia de más de dos mil miembros, celebrando tres servicios los domingos y uno los miércoles, Miércoles de Ministración. Esto ha desatado un avivamiento sobrenatural en nuestro país, en donde hemos sido testigos de restauración de familias, milagros físicos y financieros. En CIC creemos firmemente en la gran comisión de ir y hacer discípulos por medio del Modelo de Jesús.
+			En estos ultimos años extendemos nuestras estacas y logramos abrir sedes en Japon y diferentes partes de Bogotá. Hoy más de 1000 personas se reúnen cada viernes y domingo, experimentando el gran poder y amor de Dios en sus vidas.
+			</h4>
+		</div>
+	</div>
+	
+	<div class="institutolife">
+		<div class="logo-life wow animated fadeInLeft"><img src="/img/institutolife.png" width="100%"></div>
+		<h3 class="text white light center wow animated bounceInUp">Instituto Life es un centro de liderazgo que impacta de manera sobrenatural las cuatro principales áreas del ser humano: personal, relacional, ministerial y de proyección social. Es una entidad de formación que busca el desarrollo de las personas a través de su encuentro con Jesús y de la permanencia a Su lado.</h3>
+		<a href="http://institutolife.co/" style='text-decoration:none;'>
+			<p id="btn-life">Ingresar</p>
+		</a>
+	</div>
+
+	<footer>
+		<?php include ('footer/footer.html');?>
+	</footer>
+
+</body>
+</html>
